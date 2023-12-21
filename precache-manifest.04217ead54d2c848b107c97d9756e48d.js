13f4159e3c89506635be0861e4f3d98da394016b
brandonele <bele@ucsd.edu>
2023-12-21 02:01:28 -0800

    Updates

---
+++ precache-manifest.04217ead54d2c848b107c97d9756e48d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8cef87997049f0e9ebc98749a1414a1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eb16d5a3c2622104d829",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "ced08008ff2dcfe76100",
+    "url": "./static/css/main.846f2a22.chunk.css"
+  },
+  {
+    "revision": "eb16d5a3c2622104d829",
+    "url": "./static/js/2.928ccd65.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.928ccd65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ced08008ff2dcfe76100",
+    "url": "./static/js/main.dfcd7a53.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
